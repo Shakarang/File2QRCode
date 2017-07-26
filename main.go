ea446a765f3b3110d68214da51ea4b5870967718
Maxime Junger <maxime@junger.io>
2017-07-26 18:33:50 +0200

    Command line helper Added a command line interface to have multiple actions. Uses https://github.com/urfave/cli

--- main.go
+++ main.go
@@ -8,31 +8,61 @@ import (
 	"strings"
 
 	qrcode "github.com/skip2/go-qrcode"
+	"github.com/urfave/cli"
 )
 
 const limit = 1400
 
 func main() {
-	data := getFileData(os.Args[1])
-	subs := splitIntoSubstrings(data)
-	createCodesFromStrings(subs)
+
+	var filePath string
+	var destination string
+
+	app := cli.NewApp()
+	app.Name = "File2QRCode"
+	app.Usage = "Split a file into multiple QR Codes"
+	app.Version = "0.0.1"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "split",
+			Usage:       "Split the given file into multiple QR Codes",
+			Destination: &filePath,
+		},
+		cli.StringFlag{
+			Name:        "output",
+			Usage:       "Specify a different output (default is current directory)",
+			Destination: &destination,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if c.String("split") == filePath {
+			data := getFileData(filePath)
+			subs := splitIntoSubstrings(data)
+			createCodesFromStrings(subs, &destination)
+		}
+		return nil
+	}
+
+	app.Run(os.Args)
 }
 
-func getFileData(filename string) []byte {
+func getFileData(filename string) *[]byte {
 
 	fileData, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic("The file given cannot be opened.")
 	}
-	return fileData
+	return &fileData
 }
 
-func splitIntoSubstrings(data []byte) []string {
+func splitIntoSubstrings(data *[]byte) *[]string {
 
 	var content []string
 	var tmpString string
 
-	splitted := strings.Split(string(data), "\n")
+	splitted := strings.Split(string(*data), "\n")
 
 	for _, s := range splitted {
 		if (len(tmpString) + len(s)) < limit {
@@ -43,12 +73,12 @@ func splitIntoSubstrings(data []byte) []string {
 		tmpString = ""
 	}
 
-	return content
+	return &content
 }
 
-func createCodesFromStrings(data []string) {
+func createCodesFromStrings(data *[]string, destination *string) {
 
-	for index, element := range data {
+	for index, element := range *data {
 		data := models.Data{ID: index, Content: element}
 		code := models.Code{
 			Data:     data,
@@ -58,6 +88,16 @@ func createCodesFromStrings(data []string) {
 		if err != nil {
 			panic(err.Error())
 		}
-		ioutil.WriteFile(fmt.Sprintf("%v.png", index+1), pngData, 0644)
+		if *destination != "" {
+			fmt.Println("Destination", *destination)
+			if err := ioutil.WriteFile(fmt.Sprintf("%v/%v.png", *destination, index+1), pngData, 0644); err != nil {
+				panic(err)
+			}
+		} else {
+			fmt.Println("Destination NIL")
+			if err := ioutil.WriteFile(fmt.Sprintf("%v.png", index+1), pngData, 0644); err != nil {
+				panic(err)
+			}
+		}
 	}
 }
