af3f66db333132eb67ef79cda6dab4858fb71235
Maxime Junger <maxime@junger.io>
2017-10-21 18:13:45 +0200

    iOS project initialisation

---
+++ iOS/File2QRCode/ViewControllers/MainViewController/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  File2QRCode
+//
+//  Created by Maxime Junger on 21/10/2017.
+//  Copyright © 2017 Maxime Junger. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+	// MARK: - View
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK: - Builder
+extension MainViewController {
+
+	class func build() -> MainViewController {
+		return MainViewController(nibName: "MainViewController", bundle: nil)
+	}
+}
