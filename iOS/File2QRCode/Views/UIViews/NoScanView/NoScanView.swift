5b46ce89cc4ec7c1bde020310068a5bcfaab0ddc
Maxime Junger <maxime@junger.io>
2017-10-24 21:26:23 +0200

    Main view

--- iOS/File2QRCode/Views/UIViews/NoScanView/NoScanView.swift
+++ iOS/File2QRCode/Views/UIViews/NoScanView/NoScanView.swift
@@ -7,21 +7,57 @@
 //
 
 import UIKit
+import SafariServices
 
 class NoScanView: UIView {
+
+	@IBOutlet weak var descriptionLabel: UILabel!
+	@IBOutlet weak var githubLinkLabel: UILabel!
+
+	private let githubRepoString = "https://github.com/Shakarang/File2QRCode"
 
 	init() {
 		super.init(frame: CGRect.zero)
 		fromNib()
+		self.initElements()
 	}
 
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		fromNib()
+		self.initElements()
 	}
 
 	required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
 		fromNib()
+		self.initElements()
+	}
+
+	private func initElements() {
+
+		// Description
+		self.descriptionLabel.text = "Scan all your QR codes generated with File2QRCode desktop application in order to retrieve your entire file."
+
+
+		// Github link
+		let underlineAttribute = [NSAttributedStringKey.underlineStyle: NSUnderlineStyle.styleSingle.rawValue]
+		let githubLinkString = NSAttributedString(string: self.githubRepoString, attributes: underlineAttribute)
+		self.githubLinkLabel.attributedText = githubLinkString
+		self.githubLinkLabel.font = self.githubLinkLabel.font.withSize(13)
+
+		let tapGesture = UITapGestureRecognizer(target: self, action: #selector(goToRepo))
+		self.githubLinkLabel.isUserInteractionEnabled = true
+		self.githubLinkLabel.addGestureRecognizer(tapGesture)
+	}
+
+	@objc private func goToRepo() {
+		let svc = SFSafariViewController(url: URL(string: self.githubRepoString)!)
+
+		var topVC = UIApplication.shared.keyWindow?.rootViewController
+		while((topVC!.presentedViewController) != nil) {
+			topVC = topVC!.presentedViewController
+		}
+		topVC?.present(svc, animated: true, completion: nil)
 	}
 }
